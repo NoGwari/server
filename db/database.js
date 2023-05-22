f9455fb769bece869253f96e9f29e23257c51859
KKardd <wlstjd4013@gmail.com>
2023-05-22 21:54:00 +0900

    feat: init sequelize

---
+++ db/database.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2";
+import SQ from "sequelize";
+import {config} from "../config.js";
+
+// sequelize
+const {host, user, database, password} = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+    host,
+    dialect: "mysql",
+    logging: false,
+});
