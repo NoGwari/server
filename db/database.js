22f3193154017ba795b9637f8e4cf82382ffa1bc
KKardd <wlstjd4013@gmail.com>
2023-05-23 17:27:04 +0900

    feat: init sequelize to rds

--- db/database.js
+++ db/database.js
@@ -3,6 +3,7 @@ import SQ from "sequelize";
 import {config} from "../config.js";
 
 // sequelize
+
 const {host, user, database, password} = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
