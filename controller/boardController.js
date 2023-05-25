b71738d4176904f3482712799a12d05d0d5afe37
KKardd <wlstjd4013@gmail.com>
2023-05-26 02:50:30 +0900

    feat: add data create post function

--- controller/boardController.js
+++ controller/boardController.js
@@ -1,8 +1,8 @@
 import * as boardRepository from "../data/boardData.js";
 
 export async function getPostings(req, res) {
-    const username = req.query.username;
-    const data = await (username ? boardRepository.getAllByUsername(username) : boardRepository.getAll());
+    const nickName = req.query.username;
+    const data = await (nickName ? boardRepository.getAllByUsername(nickName) : boardRepository.getAll());
     res.status(200).json(data);
 }
 
@@ -17,7 +17,7 @@ export async function getPosting(req, res) {
 }
 
 export async function newPostings(req, res) {
-    const {title, content, username, userId} = req.body;
-    const newposts = await boardRepository.create(title, content, username, userId);
+    const {title, content, hiddenNum, userId, categoryId} = req.body;
+    const newposts = await boardRepository.create(title, content, hiddenNum, userId, categoryId);
     res.status(200).json(newposts);
 }
