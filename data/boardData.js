37f3efa6f154de5c7b47d1e2ce136d1a7fe69494
KKardd <wlstjd4013@gmail.com>
2023-05-25 17:52:44 +0900

    feat: join table

--- data/boardData.js
+++ data/boardData.js
@@ -1,124 +1,136 @@
 import SQ, {TEXT} from "sequelize";
 import {sequelize} from "../db/database.js";
+import {Category} from "./boardCategory.js";
+import {User} from "./user.js";
+import {Reply} from "./boardReply.js";
+import {Img} from "./boardimg.js";
 
 const Sequelize = SQ.Sequelize;
 const DateTypes = SQ.DataTypes;
 
+// define을 쓰면 자동으로 테이블을 만드는데 난 왜 일일이 만들고 그렇게 했을까?
+// 스키마를 정의하는 과정에서 테이블명을 빼는게 좋은 이유?
 const Board = sequelize.define("board", {
-    BoardID: {
+    id: {
         type: DateTypes.INTEGER,
         primaryKey: true,
         unique: true,
         autoIncrement: true,
         allowNull: false,
     },
-    BoardTitle: {
-        type: DateTypes.CHAR(20),
+    title: {
+        type: DateTypes.STRING(20),
         allowNull: false,
     },
-    BoardContent: {
+    content: {
         type: DateTypes.TEXT,
         allowNull: false,
     },
-    BoardViews: {
-        type: DateTypes.INTEGER,
+    views: {
+        type: DateTypes.INTEGER.ZEROFILL,
         unique: true,
         allowNull: false,
     },
-    BoardHits: {
-        type: DateTypes.INTEGER,
+    hits: {
+        type: DateTypes.INTEGER.ZEROFILL,
         unique: true,
         allowNull: false,
     },
-    BoardDislikes: {
-        type: DateTypes.INTEGER,
+    dislikes: {
+        type: DateTypes.INTEGER.ZEROFILL,
         unique: true,
         allowNull: false,
     },
-    BoardReported: {
-        type: DateTypes.INTEGER,
+    reported: {
+        type: DateTypes.INTEGER.ZEROFILL,
         unique: true,
         allowNull: false,
     },
-    BoardHidden: {
-        type: DateTypes.CHAR(1),
+    hidden: {
+        type: DateTypes.STRING(1),
         allowNull: false,
-    },
-    BoardCreatedAt: {
-        type: DateTypes.DATE,
-        allowNull: false,
-    },
-    BoardUpdatedAt: {
-        type: DateTypes.DATE,
-        allowNull: true,
     },
 });
 
-// Board.belongsTo(User);
+// Board User 일대일관계
+User.hasOne(Board);
+Board.belongsTo(User);
+// Board Category 일대일관계
+Category.hasOne(Board);
+Board.belongsTo(Category);
+// Board Reply 일대다관계
+Board.hasMany(Reply);
+Reply.belongsTo(Board);
+// Board Img 일대다관계
+Board.hasMany(Img);
+Img.belongsTo(Board);
 
-// const INCLUDED_USER = {
-//     attributes: [
-//         "BoardID",
-//         "BoardTitle",
-//         "BoardContent",
-//         "BoardHits",
-//         "BoardViews",
-//         "BoardDislikes",
-//         "BoardReported",
-//         "BoardHidden",
-//         "BoardCreatedAt",
-//         "BoardUpdatedAt",
-//         [Sequelize.col("user.UserIDtoNum"), "UserIDtoNum"],
-//         [Sequelize.col("user.UserNickname"), "UserNickname"],
-//         [Sequelize.col("user.UserImg"), "UserImg"],
-//         [Sequelize.col("user.UserGrade"), "UserGrade"],
-//     ],
-//     include: {
-//         model: User,
-//         attributes: [],
-//     },
-// };
-
-const attributes = {
+const INCLUDED_ALL = {
     attributes: [
-        "BoardID",
-        "BoardTitle",
-        "BoardContent",
-        "BoardHits",
-        "BoardViews",
-        "BoardDislikes",
-        "BoardReported",
-        "BoardHidden",
-        "BoardCreatedAt",
-        "BoardUpdatedAt",
+        "id",
+        "title",
+        "content",
+        "hits",
+        "views",
+        "dislikes",
+        "reported",
+        "hidden",
+        "createdAt",
+        "updatedAt",
+        "userId",
+        "categoryId",
+        [Sequelize.col("user.nickname"), "nickname"],
+        [Sequelize.col("user.img"), "img"],
+        [Sequelize.col("user.grade"), "grade"],
+        [Sequelize.col("category.name"), "name"],
+    ],
+    include: [
+        {
+            model: User,
+            attributes: [],
+        }, // model을 두개 정의하는 법을 이렇게 하면 되는구나!
+        {
+            model: Category,
+            attributes: [],
+        },
     ],
 };
 
-const ORDER_DESC = {order: [["BoardCreatedAt", "DESC"]]};
+const ORDER_DESC = {order: [["createdAt", "DESC"]]};
 
 export async function getAll() {
-    return Board.findAll({...attributes, ...ORDER_DESC});
+    return Board.findAll({
+        ...INCLUDED_ALL,
+        ...ORDER_DESC,
+    });
 }
 
 export async function getAllByUsername(username) {
-    const arr = posting.filter((post) => post.username === username);
-    return arr;
+    return Board.findAll({
+        ...INCLUDED_ALL,
+        ...ORDER_DESC,
+        include: {
+            where: {username},
+        },
+    });
 }
 
 export async function getById(id) {
-    const posts = posting.find((post) => post.id === parseInt(id));
-    return posts;
+    return Board.findOne({
+        ...INCLUDED_ALL,
+        include: {
+            where: {id},
+        },
+    });
 }
 
-export async function create(title, content, username, userId) {
-    const newPosts = {
-        id: posting.length + 1,
-        title,
-        content,
-        username,
-        userId,
-        createdAt: new Date().toString(),
-    };
-    posting.push(newPosts);
-    return newPosts;
+export async function create(title, content, hiddenNum, username, categoryId) {
+    return Board.create({
+        BoardTitle: title,
+        BoardContent: content,
+        BoardHidden: hiddenNum,
+    }).then((result) => {
+        console.log(title, content);
+        return getById(result.dataValues.id);
+    });
 }
