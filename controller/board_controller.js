478c3eeef5a63b5f37358a55fc39735290c419b5
KKardd <wlstjd4013@gmail.com>
2023-06-11 18:54:14 +0900

    docs: todo annotation to put&delete function

--- controller/board_controller.js
+++ controller/board_controller.js
@@ -73,7 +73,7 @@ export async function updatePost(req, res) {
     if (!post) {
         res.status(404).json(id);
     }
-    //fix userId 확인절차 추가해야함
+    //TODO userId 확인절차 추가해야함
     const updatePosts = await boardRepository.update(id, title, content, hiddenNum, categoryId);
     res.status(200).json(updatePosts);
 }
@@ -84,7 +84,7 @@ export async function deletePost(req, res) {
     if (!post) {
         res.status(404).json(id);
     }
-    //fix userId 확인절차 추가해야함
+    //TODO userId 확인절차 추가해야함
     const deletePosts = await boardRepository.remove(id);
     res.status(200).json(deletePosts);
 }
