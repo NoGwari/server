181c1a4ffa5a80b56e9c075e2a5544c835ccada6
KKardd <wlstjd4013@gmail.com>
2023-06-18 23:04:17 +0900

    style: remove annotations to category

--- router/category_router.js
+++ router/category_router.js
@@ -4,101 +4,13 @@ import * as categoryController from "../controller/category_controller.js";
 import {isAdimin, isAuth} from "../middleware/auth_middleware.js";
 const router = express.Router();
 
-// Category difine
-/**
- * @swagger
- * definitions:
- *   Category:
- *     properties:
- *       id:
- *         type: integer
- *       name:
- *         type: string
- *       post_num:
- *         type: integer
- */
-
-/**
- * @swagger
- * tags:
- *   - name: Category
- *     description: Category api
- */
-
 // GET /category
-/**
- * @swagger
- * paths:
- *  /Category:
- *    get:
- *      summary: "전체 게시글 조회"
- *      tags: [Category]
- *      responses:
- *        "200":
- *          description: 카테고리 조회 성공
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/definitions/Category'
- */
-
 router.get("/", categoryController.getCategory);
 
 // POST /category
-/**
- * @swagger
- * paths:
- *  /category:
- *    post:
- *      summary: "카테고리 추가"
- *      tags: [Category]
- *      consumes:
- *        - application/json
- *      parameters:
- *        - in: body
- *          name: board
- *          schema:
- *            type: object
- *            required:
- *              - name
- *            properties:
- *              name:
- *                type: string
- *      responses:
- *        "201":
- *          description: 카테고리 추가 성공
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/definitions/Category'
- */
 router.post("/", isAuth, isAdimin, categoryController.newCategory);
 
 // DELETE /category/:id
-/**
- * @swagger
- * paths:
- *  /category/{id}:
- *    delete:
- *      summary: "단일 카테고리 삭제"
- *      tags: [Category]
- *      parameters:
- *        - in: path
- *          name: id
- *          required: true
- *          type: integer
- *          minimun: 1
- *          description: "게시글의 ID값으로 서칭"
- *      responses:
- *        "200":
- *          description: 카테고리 삭제 성공
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/definitions/Category'
- *        "404":
- *          description: 해당 게시글 미존재
- */
 router.delete("/:id", isAuth, isAdimin, categoryController.deleteCategory);
 
 export default router;
