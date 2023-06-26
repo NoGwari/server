f31d3b62bb1bdb4fd891e0c08d2d7f169dffb9c4
KKardd <wlstjd4013@gmail.com>
2023-06-26 14:12:53 +0900

    style: code formating

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,5 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import AWS from "aws-sdk";
 import {S3Client} from "@aws-sdk/client-s3";
 import shortId from "shortid";
 import {config} from "../config.js";
