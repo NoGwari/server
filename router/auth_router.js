adb5a2096aeea03970a4e967c0c209b857548c96
KKardd <wlstjd4013@gmail.com>
2023-07-04 16:55:11 +0900

    feat: add mail verify router

--- router/auth_router.js
+++ router/auth_router.js
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import passport from "../controller/auth_controller.js";
 import * as authController from "../controller/auth_controller.js";
-import {isAuth} from "../middleware/auth_middleware.js";
+import {isAuth, redisMiddleware} from "../middleware/auth_middleware.js";
 
 const router = express.Router();
 
@@ -14,6 +14,10 @@ router.post("/login", authController.login);
 
 // POST /auth/signup
 router.post("/signup", authController.signup);
+
+router.post("/mailsubmit", redisMiddleware, authController.mailSubmit);
+
+router.post("/checkkey", redisMiddleware, authController.checkVerifyKey);
 
 router.get("/google", passport.authenticate("google", {scope: ["profile", "email"]}));
 router.get("/google/callback", passport.authenticate("google", {failureRedirect: "/", session: false}), (req, res) => {
