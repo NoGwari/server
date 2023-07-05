3e33c8c20e8cd5c88de6c10c18f7237c480d7a94
KKardd <wlstjd4013@gmail.com>
2023-07-05 14:31:55 +0900

    test: init typescript test

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express, {NextFunction, Request, Response} from "express";
 import "express-async-errors";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 import categoryRouter from "./router/category_router.js";
 import boardRouter from "./router/board_router.js";
 import authRouter from "./router/auth_router.js";
-import swaggerUi from "swagger-ui-express";
 import passport from "./controller/auth_controller.js";
-import YAML from "yamljs";
 import {config} from "./config.js";
 import {sequelize} from "./db/database.js";
 
@@ -42,12 +42,12 @@ app.use("/board", boardRouter);
 app.use("/category", categoryRouter);
 app.use("/auth", authRouter);
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     console.log("Finish Not Found");
     res.sendStatus(404);
 });
 
-app.use((error, req, res, next) => {
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     console.log(error);
     res.sendStatus(500);
 });
