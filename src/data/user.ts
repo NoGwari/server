99c25b7547676d4e7b60d867d626696a3cbee61f
KKardd <wlstjd4013@gmail.com>
2023-07-10 13:24:57 +0900

    feat: apply typescript to user database definition

--- src/data/user.ts
+++ src/data/user.ts
@@ -99,58 +99,6 @@ User.init(
     }
 );
 
-// export const User = sequelize.define(
-//     "user",
-//     {
-//         id: {
-//             type: DateTypes.INTEGER,
-//             autoIncrement: true,
-//             allowNull: false,
-//             primaryKey: true,
-//         },
-//         realid: {
-//             type: DateTypes.STRING(20),
-//             allowNull: false,
-//             unique: true,
-//         },
-//         password: {
-//             type: DateTypes.STRING(100),
-//             allowNull: false,
-//         },
-//         nickname: {
-//             type: DateTypes.STRING(10),
-//             allowNull: false,
-//             unique: true,
-//         },
-//         email: {
-//             type: DateTypes.STRING(30),
-//             allowNull: false,
-//             unique: true,
-//         },
-//         grade: {
-//             type: DateTypes.STRING(10),
-//             allowNull: false,
-//         },
-//         img: {
-//             type: DateTypes.STRING(100),
-//             allowNull: true,
-//         },
-//         posting_num: {
-//             type: DateTypes.INTEGER.ZEROFILL,
-//             allowNull: false,
-//         },
-//         reply_num: {
-//             type: DateTypes.INTEGER.ZEROFILL,
-//             allowNull: false,
-//         },
-//         reported: {
-//             type: DateTypes.INTEGER.ZEROFILL,
-//             allowNull: false,
-//         },
-//     },
-//     {timestamps: false, charset: "utf8", collate: "utf8_general_ci"}
-// );
-
 export async function findByRealId(realid: string) {
     return User.findOne({where: {realid: realid}});
 }
