96f5d0024be64e8e863da0a80d056b278d701d05
KKardd <wlstjd4013@gmail.com>
2023-08-03 21:18:29 +0900

    feat: add post_num variance function

--- src/controller/board_controller.ts
+++ src/controller/board_controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import * as boardRepository from "../data/board/data.js";
 import * as hitBoardRepository from "../data/board/hit_board.js";
+import * as userRepository from "../data/user.js";
 
 // import {BoardType, BoardAttributes} from "../../customType/board.js";
 export interface BoardType {
@@ -49,17 +50,17 @@ export async function getPostingByPage(req: Request, res: Response) {
 }
 
 export async function getPosting(req: Request, res: Response) {
-    const id: number = Number(req.params.id);
-    const data: BoardAttributes | null = await boardRepository.getById(id);
+    const postId: number = Number(req.params.id);
+    const data: BoardAttributes | null = await boardRepository.getById(postId);
     if (!data) {
-        return res.status(404).json(id);
+        return res.status(404).json(postId);
     }
     if (!req.session.viewCount) {
         req.session.viewCount = [];
     }
-    if (!req.session.viewCount.includes(id)) {
-        await boardRepository.incrementViewCount(id);
-        req.session.viewCount.push(id);
+    if (!req.session.viewCount.includes(postId)) {
+        await boardRepository.incrementViewCount(postId);
+        req.session.viewCount.push(postId);
     }
     res.status(200).json(data);
 }
@@ -96,47 +97,50 @@ export async function newPosting(req: Request, res: Response) {
     const {title, content, categoryId} = req.body;
     const userId: number = req.userId!;
     const newPosts = await boardRepository.create(title, content, userId, categoryId);
+    await userRepository.incrementPostNum(userId);
     res.status(200).json(newPosts);
 }
 
 export async function updatePost(req: Request, res: Response) {
-    const id = Number(req.params.id);
+    const postId = Number(req.params.id);
     const {title, content, hiddenNum, categoryId} = req.body;
-    const post: BoardAttributes | null = await boardRepository.getById(id);
+    const post: BoardAttributes | null = await boardRepository.getById(postId);
     if (!post) {
-        return res.status(404).json(id);
+        return res.status(404).json(postId);
     }
     if (req.userId !== post.userId) {
         return res.sendStatus(403);
     }
-    const updatePosts = await boardRepository.update(id, title, content, hiddenNum, categoryId);
+    const updatePosts = await boardRepository.update(postId, title, content, hiddenNum, categoryId);
     res.status(200).json(updatePosts);
 }
 
 export async function deletePost(req: Request, res: Response) {
-    const id = Number(req.params.id);
-    const post: BoardAttributes | null = await boardRepository.getById(id);
+    const postId = Number(req.params.id);
+    const userId: number = req.userId!;
+    const post: BoardAttributes | null = await boardRepository.getById(postId);
     if (!post) {
-        return res.status(404).json(id);
+        return res.status(404).json(postId);
     }
     if (req.userId !== post!.userId) {
         return res.sendStatus(403);
     }
-    const deletePosts = await boardRepository.remove(id);
+    const deletePosts = await boardRepository.remove(postId);
+    await userRepository.decrementPostNum(userId);
     res.status(200).json(deletePosts);
 }
 
 export async function incrementHits(req: Request, res: Response) {
-    const id = Number(req.params.id);
-    const post: BoardAttributes | null = await boardRepository.getById(id);
+    const postId = Number(req.params.id);
+    const post: BoardAttributes | null = await boardRepository.getById(postId);
     if (!post) {
-        return res.status(404).json(id);
+        return res.status(404).json(postId);
     }
-    const isHits: boolean = await hitBoardRepository.isHits(id, req.userId!);
+    const isHits: boolean = await hitBoardRepository.isHits(postId, req.userId!);
     if (isHits) {
         return res.sendStatus(400);
     }
-    await hitBoardRepository.increment(id, req.userId!);
-    await boardRepository.plusHits(id);
+    await hitBoardRepository.increment(postId, req.userId!);
+    await boardRepository.plusHits(postId);
     res.sendStatus(200);
 }
