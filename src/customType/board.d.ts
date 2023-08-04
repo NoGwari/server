cc8207d36f089c1dbdfbb692c8b6f30a62bf8ed7
KKardd <wlstjd4013@gmail.com>
2023-08-04 23:03:28 +0900

    feat: add type declaration file for board

--- src/customType/board.d.ts
+++ src/customType/board.d.ts
@@ -3,7 +3,6 @@ export interface BoardType {
     content: string;
     views: number;
     hits: number;
-    dislikes: number;
     reported: number;
     hidden: string;
     userId: number;
@@ -16,7 +15,6 @@ export interface BoardAttributes extends BoardType {
     content: string;
     views: number;
     hits: number;
-    dislikes: number;
     reported: number;
     hidden: string;
     userId: number;
