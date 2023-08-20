1e08fd7a3fb83b60373dc49f16a892a84a728617
KKardd <wlstjd4013@gmail.com>
2023-08-20 18:54:27 +0900

    feat: default include init

--- src/data/board/comment.ts
+++ src/data/board/comment.ts
@@ -1,3 +1,41 @@
-import SQ from "sequelize";
+import SQ, {FindOptions} from "sequelize";
 import {CommentsAttributes, CommentsType} from "../../customType/comment.js";
-import {sequelize} from "../../db/database.js";
+import Board from "../../models/data.js";
+import User from "../../models/user.js";
+
+const Sequelize = SQ.Sequelize;
+
+const INCLUDED_ALL: FindOptions<CommentsAttributes> = {
+    attributes: [
+        "id",
+        "boardId",
+        "userId",
+        "content",
+        "hits",
+        "reported",
+        "createdAt",
+        "updatedAt",
+        [Sequelize.col("user.nickname"), "userNickname"],
+        [Sequelize.col("user.img"), "userImg"],
+        [Sequelize.col("user.grade"), "userGrade"],
+    ],
+    include: [
+        {
+            model: Board,
+            as: "board",
+            attributes: [],
+        },
+        {
+            model: User,
+            as: "user",
+            attributes: [],
+        },
+    ],
+};
+
+const ORDER_DESC: FindOptions<CommentsAttributes> = {
+    order: [
+        ["createdAt", "DESC"],
+        ["id", "DESC"],
+    ],
+};
