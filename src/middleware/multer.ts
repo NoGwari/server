9b6603223f29bae3f37e75fd9279be1c96a2ff9d
KKardd <wlstjd4013@gmail.com>
2023-08-20 21:43:57 +0900

    feat: add user img to multer-s3

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,10 +1,10 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import shortId from "shortid";
 import {S3Client} from "@aws-sdk/client-s3";
 import {config} from "../config.js";
+import shortId from "shortid";
 
-export const upload = multer({
+export const uploadPostImg = multer({
     storage: multerS3({
         s3: new S3Client({
             credentials: {
@@ -16,11 +16,32 @@ export const upload = multer({
         bucket: "nogwari2",
         acl: "public-read-write",
         contentType: multerS3.AUTO_CONTENT_TYPE,
-        key: function (req, file, cb) {
-            const fileId = shortId.generate();
+        key: function (req: any, file, cb) {
             const type = file.mimetype.split("/")[1];
-            const fileName = `${fileId}.${type}`;
-            cb(null, fileName);
+            const randomFileName = shortId.generate();
+            const filePath = `board/${randomFileName}.${type}`; // 저장될 파일 경로와 이름
+            cb(null, filePath);
+        },
+    }),
+});
+
+export const uploadUserImg = multer({
+    storage: multerS3({
+        s3: new S3Client({
+            credentials: {
+                accessKeyId: config.aws.accessKey,
+                secretAccessKey: config.aws.secretAccessKey,
+            },
+            region: "ap-northeast-2",
+        }),
+        bucket: "nogwari2",
+        acl: "public-read-write",
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req: any, file, cb) {
+            const type = file.mimetype.split("/")[1];
+            const randomFileName = shortId.generate();
+            const filePath = `user/${randomFileName}.${type}`; // 저장될 파일 경로와 이름
+            cb(null, filePath);
         },
     }),
 });
