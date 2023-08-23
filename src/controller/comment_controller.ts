78363043dc118bb286fde1a9fbf2aa7bc7ed3db2
KKardd <wlstjd4013@gmail.com>
2023-08-23 15:59:00 +0900

    fix: add 404 error conditions to comment api

--- src/controller/comment_controller.ts
+++ src/controller/comment_controller.ts
@@ -3,9 +3,14 @@ import * as userRepository from "../data/user.js";
 import * as commentRepository from "../data/board/comment.js";
 import * as boardRepository from "../data/board/data.js";
 import * as hitBoardRepository from "../data/board/hit_board.js";
+import Board from "../models/data.js";
 
 export async function getComment(req: Request, res: Response) {
     const boardId: number = Number(req.params.id);
+    const isBoard: Board | null = await boardRepository.getById(boardId);
+    if (!isBoard) {
+        return res.status(404).json(boardId);
+    }
     const comment = await commentRepository.getAllComment(boardId);
     const reply = await commentRepository.getAllReply(boardId);
     const result = {
@@ -17,18 +22,26 @@ export async function getComment(req: Request, res: Response) {
 
 export async function newComment(req: Request, res: Response) {
     const boardId: number = Number(req.params.id);
+    const isBoard: Board | null = await boardRepository.getById(boardId);
+    if (!isBoard) {
+        return res.status(404).json(boardId);
+    }
     const userId: number = req.userId!;
     const content = req.body.content;
     const createComment = await commentRepository.create(content, userId, boardId);
     await userRepository.incrementReplyNum(userId);
-    res.status(200).json(createComment);
+    res.status(201).json(createComment);
 }
 
 export async function newReply(req: Request, res: Response) {
     const boardId: number = Number(req.params.id);
+    const isBoard: Board | null = await boardRepository.getById(boardId);
+    if (!isBoard) {
+        return res.status(404).json(boardId);
+    }
     const userId: number = req.userId!;
     const {content, parentCommentId} = req.body;
     const createReply = await commentRepository.createReply(content, parentCommentId, userId, boardId);
     await userRepository.incrementReplyNum(userId);
-    res.status(200).json(createReply);
+    res.status(201).json(createReply);
 }
