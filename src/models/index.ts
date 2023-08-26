dc86a47531117cca7481bb59e7c4d98de46c9098
KKardd <wlstjd4013@gmail.com>
2023-08-26 12:05:49 +0900

    feat: declare relationship to reported db

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,8 @@ import User, {associate as associateUser} from "./user.js";
 import Category, {associate as associateCategory} from "./category.js";
 import HitBoard, {associate as associateHitBoard} from "./hit_board.js";
 import HitComment, {associate as associateHitComment} from "./hit_comment.js";
+import ReportedBoard, {associate as associateReportedBoard} from "./reported_board.js";
+import ReportedComment, {associate as associateReportedComment} from "./reported_comment.js";
 export * from "../db/database.js";
 
 const db = {
@@ -13,6 +15,8 @@ const db = {
     Board,
     HitBoard,
     HitComment,
+    ReportedBoard,
+    ReportedComment,
 };
 
 export type dbType = typeof db;
@@ -20,5 +24,8 @@ export type dbType = typeof db;
 associateBoard(db);
 associateComment(db);
 associateUser(db);
-associateHitBoard(db);
 associateCategory(db);
+associateHitBoard(db);
+associateHitComment(db);
+associateReportedBoard(db);
+associateReportedComment(db);
